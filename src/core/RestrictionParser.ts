9365883466ca9429efc4f809aa5c91befdf7da79
Jan Carlos Anaya Jiménez <janaya@unimagdalena.edu.co>
2024-05-25 10:24:25 -0500

    extract logic for parse polynomial

--- src/core/RestrictionParser.ts
+++ src/core/RestrictionParser.ts
@@ -1,3 +1,4 @@
+import PolynomialParser from './PolynomialParser';
 import Restriction from './Restriction';
 
 class RestrictionParser {
@@ -5,25 +6,7 @@ class RestrictionParser {
         const operatorMatch = restriction.match(/<=|>=|=/);
         const operator = operatorMatch ? operatorMatch[0] : '=';
         const [lhs, rhs] = restriction.split(operator).map(part => part.trim());
-
-        const termPattern = /([+-]?\s*\d*)\s*x(\d+)/g;
-        const lhsMonomials: Restriction['lhs'] = [];
-
-        let match: RegExpExecArray | null;
-        while ((match = termPattern.exec(lhs)) !== null) {
-            let coefficientStr = match[1].replace(/\s+/g, '');
-
-            if (coefficientStr === '' || coefficientStr === '+') {
-                coefficientStr = '1';
-            } else if (coefficientStr === '-') {
-                coefficientStr = '-1';
-            }
-        
-            const coefficient = parseFloat(coefficientStr);
-            const variable = match[2];
-        
-            lhsMonomials.push({ coefficient, variable });
-        }
+        const lhsMonomials: Restriction['lhs'] = PolynomialParser.parse(lhs);
 
         return {
             lhs: lhsMonomials,
